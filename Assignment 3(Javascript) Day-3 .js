067e57e807923513924d37e1ce367d6c83377853
gautampatil-groot <81516028+gautampatil-groot@users.noreply.github.com>
2021-07-09 18:32:20 +0530

    Create Assignment 3(Javascript) Day-3 .js

---
+++ Assignment 3(Javascript) Day-3 .js
@@ -0,0 +1,15 @@
+//palindromes
+
+let str = "madam knows hannah well "
+let str1 = "", str2 = ""
+  for (t of str) {
+    if (t != " ") {
+      str2 = str2 + t
+      str1 = t + str1
+    }
+    else {
+      if (str1 == str2) console.log(str1)
+      str1 = ""
+      str2 = ""
+    }
+  }
